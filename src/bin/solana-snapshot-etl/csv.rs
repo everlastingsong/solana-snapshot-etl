4f5018d324baebdc9dc5f0541f30749518f35836
yugure <98769788+everlastingsong@users.noreply.github.com>
2022-12-03 00:45:54 +0900

    init for whirlpool extract

--- src/bin/solana-snapshot-etl/csv.rs
+++ src/bin/solana-snapshot-etl/csv.rs
@@ -1,14 +1,15 @@
-use indicatif::{ProgressBar, ProgressStyle};
 use serde::Serialize;
+use solana_program::pubkey::Pubkey;
 use solana_snapshot_etl::append_vec::{AppendVec, StoredAccountMeta};
 use solana_snapshot_etl::append_vec_iter;
 use std::io::Stdout;
 use std::rc::Rc;
+use bs58;
 
 pub(crate) struct CsvDumper {
-    accounts_spinner: ProgressBar,
     writer: csv::Writer<Stdout>,
     accounts_count: u64,
+    owner: Pubkey,
 }
 
 #[derive(Serialize)]
@@ -17,31 +18,26 @@ struct Record {
     owner: String,
     data_len: u64,
     lamports: u64,
+    data: String,
 }
 
 impl CsvDumper {
-    pub(crate) fn new() -> Self {
-        let spinner_style = ProgressStyle::with_template(
-            "{prefix:>10.bold.dim} {spinner} rate={per_sec}/s total={human_pos}",
-        )
-        .unwrap();
-        let accounts_spinner = ProgressBar::new_spinner()
-            .with_style(spinner_style)
-            .with_prefix("accs");
-
+    pub(crate) fn new(owner: Pubkey) -> Self {
         let writer = csv::Writer::from_writer(std::io::stdout());
 
         Self {
-            accounts_spinner,
             writer,
             accounts_count: 0,
+            owner,
         }
     }
 
     pub(crate) fn dump_append_vec(&mut self, append_vec: AppendVec) {
         for account in append_vec_iter(Rc::new(append_vec)) {
             let account = account.access().unwrap();
-            self.dump_account(account);
+            if account.account_meta.owner.eq(&self.owner) {
+                self.dump_account(account);
+            }
         }
     }
 
@@ -51,19 +47,16 @@ impl CsvDumper {
             owner: account.account_meta.owner.to_string(),
             data_len: account.meta.data_len,
             lamports: account.account_meta.lamports,
+            data: bs58::encode(account.data).into_string(),
         };
         if self.writer.serialize(record).is_err() {
             std::process::exit(1); // if stdout closes, silently exit
         }
         self.accounts_count += 1;
-        if self.accounts_count % 1024 == 0 {
-            self.accounts_spinner.set_position(self.accounts_count);
-        }
     }
 }
 
 impl Drop for CsvDumper {
     fn drop(&mut self) {
-        self.accounts_spinner.finish();
     }
 }
--- src/bin/solana-snapshot-etl/main.rs
+++ src/bin/solana-snapshot-etl/main.rs
@@ -1,14 +1,11 @@
 use crate::csv::CsvDumper;
-use crate::geyser::GeyserDumper;
-use crate::geyser_plugin::load_plugin;
-use crate::programs::ProgramDumper;
-use crate::sqlite::SqliteIndexer;
 use clap::{ArgGroup, Parser};
 use indicatif::{ProgressBar, ProgressBarIter, ProgressStyle};
 use log::{error, info};
 use reqwest::blocking::Response;
+use solana_program::pubkey::Pubkey;
+use solana_sdk::pubkey;
 use solana_snapshot_etl::archived::ArchiveSnapshotExtractor;
-use solana_snapshot_etl::parallel::AppendVecConsumer;
 use solana_snapshot_etl::unpacked::UnpackedSnapshotExtractor;
 use solana_snapshot_etl::{AppendVecIterator, ReadProgressTracking, SnapshotExtractor};
 use std::fs::{File, OpenOptions};
@@ -16,34 +13,15 @@ use std::io::{stdout, IoSliceMut, Read, Write};
 use std::path::{Path, PathBuf};
 
 mod csv;
-mod geyser;
-mod geyser_plugin;
-mod mpl_metadata;
-mod programs;
-mod sqlite;
+
+const ORCA_WHIRLPOOL_PROGRAM_ID: Pubkey = pubkey!("whirLbMiicVdio4qvUfM5KAg6Ct8VwpYzGff3uctyCc");
+
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
-#[clap(group(
-    ArgGroup::new("action")
-        .required(true)
-        .args(&["csv", "geyser", "sqlite-out", "programs-out"]),
-))]
 struct Args {
     #[clap(help = "Snapshot source (unpacked snapshot, archive file, or HTTP link)")]
     source: String,
-    #[clap(long, action, help = "Write CSV to stdout")]
-    csv: bool,
-    #[clap(long, help = "Export to new SQLite3 DB at this path")]
-    sqlite_out: Option<String>,
-    #[clap(long, help = "SQLite3 cache size in MB")]
-    sqlite_cache_size: Option<i64>,
-    #[clap(long, action, help = "Index token program data")]
-    tokens: bool,
-    #[clap(long, help = "Load Geyser plugin from given config file")]
-    geyser: Option<String>,
-    #[clap(long, help = "Write programs tar stream")]
-    programs_out: Option<String>,
 }
 
 fn main() {
@@ -59,65 +37,15 @@ fn main() {
 fn _main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
     let mut loader = SupportedLoader::new(&args.source, Box::new(LoadProgressTracking {}))?;
-    if args.csv {
-        info!("Dumping to CSV");
-        let mut writer = CsvDumper::new();
-        for append_vec in loader.iter() {
-            writer.dump_append_vec(append_vec?);
-        }
-        drop(writer);
-        println!("Done!");
-    }
-    if let Some(geyser_config_path) = args.geyser {
-        info!("Dumping to Geyser plugin: {}", &geyser_config_path);
-        let plugin = unsafe { load_plugin(&geyser_config_path)? };
-        assert!(
-            plugin.account_data_notifications_enabled(),
-            "Geyser plugin does not accept account data notifications"
-        );
-        let mut dumper = GeyserDumper::new(plugin);
-        for append_vec in loader.iter() {
-            dumper.on_append_vec(append_vec?)?;
-        }
-        drop(dumper);
-        println!("Done!");
-    }
-    if let Some(sqlite_out_path) = args.sqlite_out {
-        info!("Dumping to SQLite3: {}", &sqlite_out_path);
-        let db_path = PathBuf::from(sqlite_out_path);
-        if db_path.exists() {
-            return Err("Refusing to overwrite database that already exists".into());
-        }
 
-        let mut indexer = SqliteIndexer::new(db_path)?;
-        if let Some(cache_size) = args.sqlite_cache_size {
-            indexer.set_cache_size(cache_size)?;
-        }
-        let stats = indexer.insert_all(loader.iter())?;
+    info!("Dumping to CSV");
+    let mut writer = CsvDumper::new(ORCA_WHIRLPOOL_PROGRAM_ID);
+    for append_vec in loader.iter() {
+        writer.dump_append_vec(append_vec?);
+    }
+    drop(writer);
+    println!("Done!");
 
-        info!("Done!");
-        info!("Dumped {} accounts", stats.accounts_total);
-        info!("Dumped {} token accounts", stats.token_accounts_total);
-    }
-    if let Some(programs) = args.programs_out {
-        info!("Dumping program accounts to {}", &programs);
-        let writer: Box<dyn Write> = if programs == "-" {
-            Box::new(stdout())
-        } else {
-            Box::new(
-                OpenOptions::new()
-                    .write(true)
-                    .create_new(true)
-                    .open(programs)?,
-            )
-        };
-        let mut dumper = ProgramDumper::new(writer);
-        for append_vec in loader.iter() {
-            dumper.on_append_vec(append_vec?)?;
-        }
-        drop(dumper);
-        info!("Done!");
-    }
     Ok(())
 }
 
